f27af39a9ec5222489d8125d8f205b6cb4246446
Gaurav Singh Negi <gnegi897@gmail.com>
2024-03-29 17:50:12 +0530

    project complete

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Main.css';
 import {assets} from '../../assets/assets'
+import { Context } from '../../context/Context';
 
 const Main = () => {
+
+    const {onsent,recentPrompt,showResult,loading , resultData , setInput , input} = useContext(Context);
   return (
     <div className='main'>
       <div className="nav">
@@ -11,6 +14,10 @@ const Main = () => {
       </div>
 
       <div className="main-container">
+
+        {!showResult
+        ?<>
+        
         <div className="greet">
           <p><span>Hello,</span></p>
           <p>How can i help you today?</p>
@@ -34,14 +41,31 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+        </>: <div className='result'>
+                <div className="result-title">
+                  <img src={assets.user_icon} alt="" />
+                  <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                  {
+                    loading? <div className="loader">
+                      <hr />
+                      <hr />
+                      <hr />
+                    </div>:  <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                  }
+                
+                </div>
+        </div>
+        }
 
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder='Enter the prompt here' />
+            <input onChange={(e) => setInput(e.target.value)} value={input}  type="text" placeholder='Enter the prompt here' />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              {input?<img onClick={() => onsent()}  src={assets.send_icon} alt="" />:null}
             </div>
           </div>
 
